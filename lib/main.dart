4faf1942189459173bf68a7f097054a38e024de0
Andrea Lundevaller <andrea.lundevaller@gmail.com>
2022-09-09 13:16:46 +0200

    added listview to first page

--- lib/main.dart
+++ lib/main.dart
@@ -87,10 +87,9 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             )
           ]),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
+      body:
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -105,14 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            _checkbox(),
-            _list(),
-          ],
-        ),
-      ),
+          _list(),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.indigo,
         onPressed: _incrementCounter,
